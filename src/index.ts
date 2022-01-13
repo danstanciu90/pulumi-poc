f5c81a603238dede8c28f4dc89f2029a2cf48a33
Dan Stanciu <dan.stanciu@lego.com>
2022-01-13 10:11:13 +0100

    add lambda role

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as aws from '@pulumi/aws';
 import * as awsx from '@pulumi/awsx';
-import { handler } from './lambdas/activities-get';
+import { getActivitiesLambda } from './lambdas/activities-get';
 
 const domain = 'testapi.playportal.pp-dev.apps.legogroup.io';
 const awsUsEast1 = new aws.Provider('aws-provider-us-east-1', { region: 'us-east-1' });
@@ -15,13 +15,16 @@ const sslCertificate = new aws.acm.Certificate(
 );
 
 // Create DNS record to prove to ACM that we own the domain
-const sslCertificateValidationDnsRecord = new aws.route53.Record('ssl-cert-validation-dns-record', {
-  zoneId: 'Z08201573ODBLNRIX8V6U',
-  name: sslCertificate.domainValidationOptions[0].resourceRecordName,
-  type: sslCertificate.domainValidationOptions[0].resourceRecordType,
-  records: [sslCertificate.domainValidationOptions[0].resourceRecordValue],
-  ttl: 10 * 60, // 10 minutes
-});
+const sslCertificateValidationDnsRecord = new aws.route53.Record(
+  'testapi-ssl-cert-validation-dns-record',
+  {
+    zoneId: 'Z08201573ODBLNRIX8V6U',
+    name: sslCertificate.domainValidationOptions[0].resourceRecordName,
+    type: sslCertificate.domainValidationOptions[0].resourceRecordType,
+    records: [sslCertificate.domainValidationOptions[0].resourceRecordValue],
+    ttl: 10 * 60, // 10 minutes
+  }
+);
 
 const validatedSslCertificate = new aws.acm.CertificateValidation(
   'ssl-cert-validation',
@@ -32,12 +35,12 @@ const validatedSslCertificate = new aws.acm.CertificateValidation(
   { provider: awsUsEast1 }
 );
 
-const apiDomainName = new aws.apigateway.DomainName('api-domain-name', {
+const apiDomainName = new aws.apigateway.DomainName('testapi-api-domain-name', {
   certificateArn: validatedSslCertificate.certificateArn,
   domainName: domain,
 });
 
-const dnsRecord = new aws.route53.Record('api-dns', {
+const dnsRecord = new aws.route53.Record('testapi-api-dns', {
   zoneId: 'Z08201573ODBLNRIX8V6U',
   type: 'A',
   name: domain,
@@ -51,17 +54,17 @@ const dnsRecord = new aws.route53.Record('api-dns', {
 });
 
 // create the api gateway
-const gw = new awsx.apigateway.API('test-api', {
+const gw = new awsx.apigateway.API('testapi-test-api', {
   routes: [
     {
       path: '/activities',
       method: 'GET',
-      eventHandler: handler,
+      eventHandler: getActivitiesLambda,
     },
   ],
 });
 
-const basePathMapping = new aws.apigateway.BasePathMapping('api-domain-mapping', {
+const basePathMapping = new aws.apigateway.BasePathMapping('testapi-api-domain-mapping', {
   restApi: gw.restAPI,
   stageName: gw.stage.stageName,
   domainName: apiDomainName.domainName,
--- src/lambdas/activities-get.ts
+++ src/lambdas/activities-get.ts
@@ -2,29 +2,59 @@
 /* eslint-disable no-console */
 import * as aws from '@pulumi/aws';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import * as awsSdk from 'aws-sdk';
 
-export const handler: aws.lambda.EventHandler<APIGatewayProxyEvent, APIGatewayProxyResult> = async (
-  event
-) => {
-  const pathParameters = event.pathParameters;
-  const body = event.body ? JSON.parse(event.body) : undefined;
-  const queryStringParams = event.multiValueQueryStringParameters;
+export const getActivities: aws.lambda.EventHandler<APIGatewayProxyEvent, APIGatewayProxyResult> =
+  async (event) => {
+    const pathParameters = event.pathParameters;
+    const body = event.body ? JSON.parse(event.body) : undefined;
+    const queryStringParams = event.multiValueQueryStringParameters;
 
-  console.log('Received body: ', body);
-  console.log('Received pathParameters: ', pathParameters);
+    const sm = new awsSdk.SecretsManager({ region: 'eu-west-1' });
 
-  await Promise.resolve(() => {
-    console.log('promise resolved');
-  });
+    const { SecretString } = await sm.getSecretValue({ SecretId: 'ContentStackTokens' }).promise();
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Nice job, you've done it! :D",
-      body,
-      pathParameters,
-      queryStringParams,
-      event,
-    }),
+    console.log('Received body: ', body);
+    console.log('Received pathParameters: ', pathParameters);
+
+    await Promise.resolve(() => {
+      console.log('promise resolved');
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Nice job, you've done it! :D",
+        body,
+        pathParameters,
+        queryStringParams,
+        event,
+        SecretString,
+      }),
+    };
   };
-};
+
+const lambdaRole = new aws.iam.Role('iamForLambda', {
+  assumeRolePolicy: JSON.stringify({
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Action: 'sts:AssumeRole',
+        Effect: 'Allow',
+        Sid: '',
+        Principal: {
+          Service: 'lambda.amazonaws.com',
+        },
+      },
+    ],
+  }),
+  managedPolicyArns: [
+    'arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole',
+    'arn:aws:iam::aws:policy/SecretsManagerReadWrite',
+  ],
+});
+
+export const getActivitiesLambda = new aws.lambda.CallbackFunction('get-activities-lambda', {
+  callback: getActivities,
+  role: lambdaRole,
+});
